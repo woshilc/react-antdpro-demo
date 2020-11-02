48084d3d9cc464b951055336c93abae33104b8fb
woshilc <18297715288@163.com>
2020-11-02 14:52:25 +0800

    mock test

---
+++ mock/project.js
@@ -0,0 +1,102 @@
+export default {
+  'GET /api/projects': [
+    {
+      key: '1',
+      name: '项目一',
+      participants: [
+        {
+          key: '1',
+          name: 'one',
+          avator: null,
+        },
+        {
+          key: '2',
+          name: 'two',
+          avator: null,
+        },
+        {
+          key: '3',
+          name: 'three',
+          avator: null,
+        },
+        {
+          key: '4',
+          name: 'four',
+          avator: null,
+        },
+        {
+          key: '5',
+          name: 'five',
+          avator: null,
+        },
+        {
+          key: '6',
+          name: 'six',
+          avator: null,
+        },
+        {
+          key: '7',
+          name: 'seven',
+          avator: null,
+        },
+        {
+          key: '8',
+          name: 'eight',
+          avator: null,
+        },
+      ],
+    },
+    {
+      key: '2',
+      name: '项目二',
+      participants: [
+        {
+          key: '1',
+          name: 'one',
+          avator: null,
+        },
+        {
+          key: '5',
+          name: 'five',
+          avator: null,
+        },
+        {
+          key: '6',
+          name: 'six',
+          avator: null,
+        },
+        {
+          key: '8',
+          name: 'eight',
+          avator: null,
+        },
+      ],
+    },
+    {
+      key: '3',
+      name: '项目三',
+      participants: [
+        {
+          key: '1',
+          name: 'one',
+          avator: null,
+        },
+        {
+          key: '5',
+          name: 'five',
+          avator: null,
+        },
+        {
+          key: '6',
+          name: 'six',
+          avator: null,
+        },
+        {
+          key: '8',
+          name: 'eight',
+          avator: null,
+        },
+      ],
+    },
+  ],
+};
