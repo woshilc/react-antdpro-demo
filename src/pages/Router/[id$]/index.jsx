fdf3a2b420ddddf1fd04e096292a53e5ab60f395
woshilc <18297715288@163.com>
2020-11-23 16:47:37 +0800

    no message

--- src/pages/Router/[id$]/index.jsx
+++ src/pages/Router/[id$]/index.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { Link } from 'umi';
+import { Link, router } from 'umi';
+import { Beforeunload } from 'react-beforeunload';
+import { Button } from 'antd';
 import styles from '../index.less';
 
 export default class Router extends Component {
@@ -9,16 +11,39 @@ export default class Router extends Component {
   }
 
   componentDidMount() {
-    console.log(this.props.match.params);
+    // 拦截判断是否离开当前页面
+    window.addEventListener('onunload', this.beforeunload);
   }
+
+  componentWillUnmount() {
+    // 销毁拦截判断是否离开当前页面
+    window.removeEventListener('onunload', this.beforeunload);
+  }
+
+  beforeunload = (e) => {
+    let confirmationMessage = '你确定离开此页面吗?';
+    (e || window.event).returnValue = confirmationMessage;
+    return confirmationMessage;
+  };
+
+  handleOnClick = () => {
+    window.name = '123';
+  };
 
   render() {
     return (
+      // <Beforeunload onBeforeunload={this.test}>
       <div>
-        <Link to="/router/1">id1</Link>
-        <Link to="/router/2">id2</Link>
-        <Link to="/router/3">id3</Link>
+        <div>
+          <Link to="/router/1">id1</Link>
+          <Link to="/router/2">id2</Link>
+          <Link to="/router/3">id3</Link>
+        </div>
+        <div>{window.name}</div>
+        <Button onClick={this.handleOnClick}>click</Button>
       </div>
+
+      // </Beforeunload>
     );
   }
 }
